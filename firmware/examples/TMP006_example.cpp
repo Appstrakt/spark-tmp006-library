ab6c7253979735e048070519b6dc96ad907b7a53
Geert Wille <geert@appstrakt.com>
2014-12-10 15:53:34 +0100

    Changed the exmaple to work with sparkcore and added monitoring over serial connection

--- firmware/examples/TMP006_example.cpp
+++ firmware/examples/TMP006_example.cpp
@@ -1,24 +1,30 @@
-#include "TMP006/TMP006.h"
+#include "application.h"
+#include "TMP006.h"
 
 
 uint8_t sensor1 = 0x40; // I2C address of TMP006, can be 0x40-0x47
 uint16_t samples = TMP006_CFG_8SAMPLE; // # of samples per reading, can be 1/2/4/8/16
 
-TMP006 tmp(sensor1, samples);
+TMP006 tmp;
 
 void setup()
 {
     Serial.begin(9600);
+    while (!Serial.available())  // Wait here until the user presses ENTER
+        SPARK_WLAN_Loop();        // in the Serial Terminal. Call the BG Tasks
+
+    // Begin tmp006 measuring
+    tmp.begin();
     Serial.println("TMP006 Example");
 }
 
 void loop()
 {
-    float object_temp = tmp.readObjTempC(sensor1);
+    float object_temp = tmp.readObjTempC();
     Serial.print("Object Temperature: ");
     Serial.print(object_temp); Serial.println("*C");
 
-    float sensor_temp = tmp.readDieTempC(sensor1);
+    float sensor_temp = tmp.readDieTempC();
     Serial.print("Sensor Temperature: ");
     Serial.print(sensor_temp); Serial.println("*C");
 
